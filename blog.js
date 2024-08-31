e75e71fd40c3367568d6ae1fd464a91d3e4c1fbc
huang1993 <150344345+huangkuku@users.noreply.github.com>
2024-08-31 10:02:49 +0800

    Add files via upload
    push project

---
+++ blog.js
@@ -0,0 +1,17 @@
+﻿var title = document.getElementById("title");
+var content = document.getElementById("content");
+var btn = document.getElementById("btn");
+var list = document.getElementById("list");
+
+btn.addEventListener("click", blog);
+
+function blog(){
+    list.innerHTML = `
+    <div class="article">
+        <h2>${title.value}</h2>
+        <p>${content.value}</p>
+    </div>
+    `+list.innerHTML;
+    title.value = "";
+    content.value= "";
+}
